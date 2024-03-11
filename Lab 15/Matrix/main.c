434a459bf149be3a54b2a36bfe7664c8072eb74f
Stranger3311 <dmkor5@yandex.ru>
2024-03-11 19:20:45 +0300

    11.03 last

--- Lab 15/Matrix/main.c
+++ Lab 15/Matrix/main.c
@@ -1,49 +1,17 @@
 #include <stdio.h>
 #include "Matrix_lib.h"
-/*
-void inputMatrix(matrix *m){
 
-    for (int i = 0; i < m->nCols; i++){
-        for (int j = 0; j < m->nRows; j++){
-            scanf("%d", &m->values[i][j]);
-        }
-    }
-}
 
-void inputMatrices(matrix *ms, int nMatrices){
 
-    for (int i = 0; i < nMatrices; i++){
-        inputMatrix(&ms[i]);
-    }
-}
-
-void outputMatrix(matrix m){
-
-    printf("{");
-    for (int i = 0; i < m.nCols; i++){
-        for (int j = 0; j < m.nRows; j++){
-            printf("%d ", m.values[i][j]);
-        }
-        //printf("\n");
-    }
-    printf("}");
-}
-
-void outputMatrices(matrix *ms, int nMatrices){
-
-    for (int i = 0; i < nMatrices; i++){
-        outputMatrix(ms[i]);
-    }
-
-}
-*/
 int main(){
 
-    matrix a = *getMemArrayOfMatrices(2,2,2);
+    //matrix a = *getMemArrayOfMatrices(2,2,2);
 
-    inputMatrices(&a,2);
+    //inputMatrices(&a,2);
 
-    outputMatrices(&a,2);
+    //outputMatrices(&a,2);
+
+
 
 
     //freeMemMatrices(&a,2);
@@ -51,6 +19,8 @@ int main(){
     //matrix a = getMemMatrix(2,2);
 
     //inputMatrix(&a);
+
+    //selectionSortColsMatrixByColCriteria(&a,getSum());
 
     //swapRows(a,0,1);
     //swapColumns(a,0,1);

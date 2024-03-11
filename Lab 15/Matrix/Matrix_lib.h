5fccdaf76865f13a9e9a9b6bdb33e36806f35a5f
Stranger3311 <dmkor5@yandex.ru>
2024-03-11 18:44:10 +0300

    maybe work

--- Lab 15/Matrix/Matrix_lib.h
+++ Lab 15/Matrix/Matrix_lib.h
@@ -1,6 +1,31 @@
 #ifndef MATRIX_LIB_H_INCLUDED
 #define MATRIX_LIB_H_INCLUDED
 
+#include <stdio.h>
+#include <stdbool.h>
+
+void swap(int *a, int *b){
+
+    int *temp = a;
+    a = b;
+    b = temp;
+
+}
+
+size_t getMaxElementIndexInArray(const int a[], size_t n) {
+
+    int maxIndex = 0;
+
+    for(size_t i = 1; i < n; i++){
+        if(a[i] > a[maxIndex]){
+        maxIndex = i;
+        }
+    }
+
+    return maxIndex;
+}
+
+
 typedef struct matrix{
     int **values; // элементы матрицы
     int nRows; // количество рядов
@@ -62,10 +87,8 @@ void inputMatrix(matrix *m){
 void inputMatrices(matrix *ms, int nMatrices){
 
     for (int i = 0; i < nMatrices; i++){
-        //inputMatrix(&ms->values[i]);
         inputMatrix(&ms[i]);
     }
-
 }
 
 /*
@@ -93,6 +116,14 @@ void outputMatrix(matrix m){
     printf("}");
 }
 
+void outputMatrices(matrix *ms, int nMatrices){
+
+    for (int i = 0; i < nMatrices; i++){
+        outputMatrix(ms[i]);
+    }
+
+}
+
 void swapRows(matrix m, int n1, int n2){
 
     int temp = m.values[n1];
@@ -109,4 +140,73 @@ void swapColumns(matrix m, int j1, int j2){
     }
 }
 
+int getSum(int *a, int n){
+    int sum = 0;
+    for (int i = 0; i < n; i++){
+        sum += a[i];
+    }
+
+    return sum;
+}
+
+void insertionSortRowsMatrixByCriteria(matrix m, int (*criteria)(int*, int)){
+
+    int *values = malloc(sizeof(int) * m.nRows);
+
+    for (size_t i = 0; i < m.nRows; i++){
+        values[i] = criteria(m.values[i], m.nCols);
+    }
+
+    for (size_t i = 1; i < m.nRows; i++){
+
+        int j = i - 1;
+
+        while (values[i] < values[j] && j >= 0) {
+            values[j + 1] = values[j];
+            swapRows(m, j + 1, j);
+            --j;
+        }
+
+    values[j + 1] = values[i];
+    }
+
+}
+
+void selectionSortColsMatrixByColCriteria(matrix m, int (*criteria)(int*, int)) {
+    int *values = malloc(sizeof(int) * m.nCols);
+
+    for (size_t i = 0; i < m.nCols; i++) {
+        int *col = malloc( sizeof(int) * m.nRows);
+
+    for (size_t j = 0; j < m.nRows; j++)
+        col[j] = m.values[j][i];
+
+        values[i] = criteria(col, m.nRows);
+    }
+
+    for (int i = m.nCols - 1; i > 0; --i) {
+
+        int max = getMaxElementIndexInArray(values, i + 1);
+
+        if (max != i) {
+        swap(values + max, values + i);
+
+        for (size_t j = 0; j < m.nRows; j++)
+            swap(&m.values[j][max], &m.values[j][i]);
+        }
+    }
+}
+
+bool isSquareMatrix(matrix *m){
+
+    return m->nCols == m->nRows;
+
+}
+/*
+bool areTwoMatrixEqual(matrix){
+
+    for (int i )
+
+}
+*/
 #endif // MATRIX_LIB_H_INCLUDED
--- Lab 15/Matrix/main.c
+++ Lab 15/Matrix/main.c
@@ -1,25 +1,65 @@
 #include <stdio.h>
 #include "Matrix_lib.h"
+/*
+void inputMatrix(matrix *m){
 
+    for (int i = 0; i < m->nCols; i++){
+        for (int j = 0; j < m->nRows; j++){
+            scanf("%d", &m->values[i][j]);
+        }
+    }
+}
 
+void inputMatrices(matrix *ms, int nMatrices){
 
+    for (int i = 0; i < nMatrices; i++){
+        inputMatrix(&ms[i]);
+    }
+}
+
+void outputMatrix(matrix m){
+
+    printf("{");
+    for (int i = 0; i < m.nCols; i++){
+        for (int j = 0; j < m.nRows; j++){
+            printf("%d ", m.values[i][j]);
+        }
+        //printf("\n");
+    }
+    printf("}");
+}
+
+void outputMatrices(matrix *ms, int nMatrices){
+
+    for (int i = 0; i < nMatrices; i++){
+        outputMatrix(ms[i]);
+    }
+
+}
+*/
 int main(){
 
-    //matrix a = *getMemArrayOfMatrices(2,2,2);
+    matrix a = *getMemArrayOfMatrices(2,2,2);
 
-    //inputMatrices(&a,2);
+    inputMatrices(&a,2);
+
+    outputMatrices(&a,2);
 
 
     //freeMemMatrices(&a,2);
 
-    matrix a = getMemMatrix(2,2);
+    //matrix a = getMemMatrix(2,2);
 
-    inputMatrix(&a);
+    //inputMatrix(&a);
 
     //swapRows(a,0,1);
-    swapColumns(a,0,1);
+    //swapColumns(a,0,1);
 
-    outputMatrix(a);
+    //outputMatrix(a);
+
+    //printf(" %d", isSquareMatrix(&a));
+
+    //freeMemMatrix(&a);
 
 
 

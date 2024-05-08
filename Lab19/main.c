783b10c07effa7af86ef511a896a588c8d082fb9
Stranger3311 <dmkor5@yandex.ru>
2024-05-08 15:38:58 +0300

    task 3

--- Lab19/main.c
+++ Lab19/main.c
@@ -4,6 +4,7 @@
 #include <assert.h>
 #include "String_lib.h"
 #include "Void_Vector_bibl.h"
+#include <stdbool.h>
 
 //задание 1
 void transpose_matrix_in_file(const char* filename) {
@@ -279,8 +280,131 @@ void test_convert_float() {
     test_convert_float_3_three_numbers();
 }
 
+void calculate_expression(const char* filename) {
+    FILE* file = fopen(filename, "r+");
+    if (file == NULL) {
+        printf("reading error\n");
+        exit(1);
+    }
+
+    int x1, x2, x3;
+    char op1, op2;
+    char open_bracket, close_bracket;
+    float result;
+
+    int amount_element = fscanf(file, "%c%d %c %d%c %c %d", &open_bracket, &x1, &op1, &x2, &close_bracket, &op2, &x3);
+
+    bool two_operation = amount_element == 7 ? true : false;
+
+    switch (op1) {
+        case '+':
+            result = x1 + x2;
+            break;
+        case '-':
+            result = x1 -x2;
+            break;
+        case '*':
+            result = x1 * x2;
+            break;
+        case '/':
+            if (x2 == 0) {
+                fprintf(stderr, "Zero division");
+                exit(1);
+            }
+            result = x1 / x2;
+            break;
+        default:
+            fprintf(stderr,"unknown operation");
+            exit(1);
+    }
+
+    if (two_operation) {
+        switch (op2) {
+            case '+':
+                result += x3;
+                break;
+            case '-':
+                result -= x3;
+                break;
+            case '*':
+                result *= x3;
+                break;
+            case '/':
+                if (x3 == 0) {
+                    fprintf(stderr, "Zero division");
+                    exit(1);
+                }
+                result /= x3;
+                break;
+            default:
+                fprintf(stderr,"unknown operation");
+                exit(1);
+        }
+    }
+
+    fseek(file, 0, SEEK_END);
+    fprintf(file, " = %.2f", result);
+
+    fclose(file);
+}
+
+void test_calculate_expression_2_operand() {
+    const char filename[] = "D:\\GitHub\\OP\\Lab19\\task_3_test_1.txt";
+
+    char expression[] = "(2 * 4)";
+    FILE* file = fopen(filename, "w");
+
+    fputs(expression, file);
+
+    fclose(file);
+
+    calculate_expression("D:\\GitHub\\OP\\Lab19\\task_3_test_1.txt");
+
+    file = fopen(filename, "r");
+
+    char data[100] = "";
+    fgets(data, sizeof(data), file);
+
+    fclose(file);
+
+    char res[] = "(2 * 4) = 8.00 ";
+
+    assert(strcmp(data, res));
+}
+
+void test_calculate_expression_3_operand() {
+    const char filename[] = "D:\\GitHub\\OP\\Lab19\\task_3_test_2.txt";
+
+    char expression[] = "(2 * 8) + 3";
+    FILE* file = fopen(filename, "w");
+
+    fputs(expression, file);
+
+    fclose(file);
+
+    calculate_expression("D:\\GitHub\\OP\\Lab19\\task_3_test_2.txt");
+
+    file = fopen(filename, "r");
+
+    char data[100] = "";
+    fgets(data, sizeof(data), file);
+
+    fclose(file);
+
+    char res[] = "(2 * 3) + 3 = 19.00 ";
+
+    assert(strcmp(data, res));
+}
+
+
+void test_calculate() {
+    test_calculate_expression_2_operand();
+    test_calculate_expression_3_operand();
+}
+
 
 int main(){
-    test_convert_float();
+    //test_convert_float();
     //test_matrix_transpose();
+    test_calculate();
 }

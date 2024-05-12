d7bfc544fbaa146a1bdfee080971ed129cf0cfdb
Stranger3311 <dmkor5@yandex.ru>
2024-05-12 11:23:40 +0300

    9

--- Lab19/main.c
+++ Lab19/main.c
@@ -1171,6 +1171,137 @@ void test_transpose_non_symmetric_matrix() {
     test_transpose_non_symmetric_matrix_3_non_symmetric_matrix();
 }
 
+//задание 9
+#define MAX_AMOUNT_SPORTSMAN 20
+
+typedef struct sportsman {
+    char name[MAX_STRING_SIZE];
+    double best_result;
+} sportsman;
+
+void sort_sportsman(sportsman sm[], const int n) {
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if (sm[j].best_result < sm[j + 1].best_result) {
+                sportsman temp = sm[j];
+                sm[j] = sm[j + 1];
+                sm[j + 1] = temp;
+            }
+}
+
+void get_best_team(const char* filename, const int n) {
+    FILE* file = fopen(filename, "rb");
+    if (file == NULL) {
+        printf("reading error\n");
+        exit(1);
+    }
+
+    sportsman* team = (sportsman*) malloc(MAX_AMOUNT_SPORTSMAN * sizeof(sportsman));
+
+    sportsman* rec_ptr = team;
+    int amount_sportsman = 0;
+    while (fread(rec_ptr, sizeof(sportsman), 1, file) == 1) {
+        rec_ptr ++;
+        amount_sportsman++;
+    }
+
+    fclose(file);
+
+    file = fopen(filename, "wb");
+    if (file == NULL) {
+        printf("reading error\n");
+        exit(1);
+    }
+
+    sort_sportsman(team, amount_sportsman);
+
+    int amount_in_team = amount_sportsman >= n ? n : amount_sportsman;
+
+    for (int i = 0; i < amount_in_team; i++) {
+        fwrite(team + i, sizeof(sportsman), 1, file);
+    }
+
+    free(team);
+    fclose(file);
+}
+
+void test_get_best_team_1_empty_file() {
+    const char filename[] = "D:\\GitHub\\OP\\Lab19\\task_9_test_1.txt";
+
+    FILE* file = fopen(filename, "wb");
+    fclose(file);
+
+    get_best_team(filename, 0);
+
+    file = fopen(filename, "rb");
+
+    char res[100] = "";
+    fread(res, sizeof(res), 1, file);
+
+    fclose(file);
+
+    assert(strcmp(res, "") == 0);
+}
+
+void test_get_best_team_2_n_more() {
+    const char filename[] = "D:\\GitHub\\OP\\Lab19\\task_9_test_2.txt";
+
+    FILE* file = fopen(filename, "wb");
+
+    sportsman s1 = {.best_result = 15.8, .name="first"};
+    sportsman s2 = {.best_result = 8.51,  .name="second"};
+
+    fwrite(&s1, sizeof(sportsman), 1, file);
+    fwrite(&s2, sizeof(sportsman), 1, file);
+
+    fclose(file);
+
+    get_best_team(filename, 3);
+
+    file = fopen(filename, "rb");
+
+    sportsman res_1, res_2;
+    fread(&res_1, sizeof(sportsman), 1, file);
+    fread(&res_2, sizeof(sportsman), 1, file);
+
+    fclose(file);
+
+    assert(strcmp(s1.name, res_1.name) == 0 && fabs(s1.best_result - res_1.best_result) <= 0.001);
+    assert(strcmp(s2.name, res_2.name) == 0 && fabs(s2.best_result - res_2.best_result) <= 0.001);
+}
+
+void test_get_best_team_3_n_less() {
+    const char filename[] = "D:\\GitHub\\OP\\Lab19\\task_9_test_3.txt";
+
+    FILE* file = fopen(filename, "wb");
+
+    sportsman s1 = {.best_result = 22.7, .name="first"};
+    sportsman s2 = {.best_result = 16.65,  .name="second"};
+
+    fwrite(&s1, sizeof(sportsman), 1, file);
+    fwrite(&s2, sizeof(sportsman), 1, file);
+
+    fclose(file);
+
+    get_best_team(filename, 1);
+
+    file = fopen(filename, "rb");
+
+    sportsman res_1;
+    fread(&res_1, sizeof(sportsman), 1, file);
+
+    fclose(file);
+
+    assert(strcmp(s1.name, res_1.name) == 0 && fabs(s1.best_result - res_1.best_result) <= 0.001);
+}
+
+void test_get_best_team() {
+    test_get_best_team_1_empty_file();
+    test_get_best_team_2_n_more();
+    test_get_best_team_3_n_less();
+}
+
+
 
 int main(){
     //test_convert_float();
@@ -1179,5 +1310,6 @@ int main(){
     //test_leave_longest();
     //test_remove_true_polynomial();
     //test_rearrange_numbers();
-    test_transpose_non_symmetric_matrix();
+    //test_transpose_non_symmetric_matrix();
+    test_get_best_team();
 }
